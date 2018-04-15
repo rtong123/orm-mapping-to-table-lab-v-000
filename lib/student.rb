e37bdf0528b4e987346e52d63cd0ae180b6f151d
Rebecca Tong <rtong@buffalo.edu>
2018-04-15 18:13:19 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -23,6 +23,21 @@ class Student
   end
 
   def self.drop_table
+    sql = <<-SQL
+      DROP TABLE students;
+      SQL
+    DB[:conn].execute(sql)
+  end
+
+  def save
+    sql = <<-SQL
+    INSERT INTO students (name,grade)
+    VALUES (?,?)
+    SQL
+    DB[:conn].execute(sql,self.name,self.grade)
+  end
+
+  def self.create
   end
 
 
