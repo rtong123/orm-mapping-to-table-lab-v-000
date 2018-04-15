8646d9378ad8f1d7dba769db7116905357a9e196
Rebecca Tong <rtong@buffalo.edu>
2018-04-15 18:14:15 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -34,7 +34,7 @@ class Student
     INSERT INTO students (name,grade)
     VALUES (?,?)
     SQL
-    DB[:conn].execute(sql,self.name,self.grade)
+    DB[:conn].execute(sql, self.name, self.grade)
   end
 
   def self.create
